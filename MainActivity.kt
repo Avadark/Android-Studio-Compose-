ac0d7d42b23ec4bde13e4e02fcb48566c2e5d7e1
Aditya Yadav <adiiyadav120@gmail.com>
2022-09-27 00:19:12 +0530

    ComposeArticle

--- MainActivity.kt
+++ MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Horizontal
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,42 +36,63 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Birthday("Aditya ka Birthday","- from College")
+                    ComposeArticle("Jetpack Compose tutorial",
+                        "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+                        "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.")
+
                 }
             }
         }
     }
 }
 @Composable
-fun Birthday(message: String,from: String) {
-    Box(){
-        val image= painterResource(R.drawable.androidparty)
-        Image(
-            painter = image , contentDescription = null,
-            modifier = Modifier.fillMaxHeight().fillMaxWidth(),
-            contentScale = ContentScale.Crop
-        )
+fun ComposeArticle(message1: String,message2: String,message3:String) {
         Column() {
+            val image= painterResource(R.drawable.bg_compose_background)
+            Image(
+                painter = image , contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth(),
+
+                )
             Text(
-                text = message,
-                fontSize= 30.sp,
-                modifier=Modifier.background(color = Color.Transparent).padding(start = 16.dp, top = 16.dp, end = 16.dp).fillMaxWidth().wrapContentWidth(Alignment.Start)
+                text = message1,
+                fontSize= 24.sp,
+                modifier= Modifier
+                    .background(color = Color.Transparent)
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp,bottom = 16.dp)
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.Start)
             )
             Text(
-                text = from,
-                modifier=Modifier.background(color = Color.Transparent).padding(start = 16.dp, top = 5.dp, end = 16.dp, bottom = 16.dp).fillMaxWidth().wrapContentWidth(Alignment.End),
-                fontSize= 30.sp
+                text = message2,
+                modifier= Modifier
+                    .background(color = Color.Transparent)
+                    .padding(start = 16.dp, end = 16.dp)
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.End),
+
+            )
+            Text(
+                text = message3,
+                modifier= Modifier
+                    .background(color = Color.Transparent)
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp,bottom = 16.dp)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Justify
             )
         }
     }
-}
+
 
 
 @Preview(showBackground = true,showSystemUi = true)
 @Composable
 fun BirthdayCardPreview(){
     HappyBirthdayTheme() {
-        Birthday("Aditya Ka Birthday","- from College")
+        ComposeArticle("Jetpack Compose tutorial",
+            "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.")
     }
 }
 
